a471b0d6b1b1af1d73da0c13ca82925e8fc3b50b
jmedinr <juandiego619@gmail.com>
2021-12-21 08:21:44 -0500

    Cambios en el MAIN del proyecto

--- PreguntasRespuestas/src/controller/Preguntas.java
+++ PreguntasRespuestas/src/controller/Preguntas.java
@@ -5,6 +5,7 @@
  */
 package controller;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import model.*;
@@ -17,8 +18,18 @@ import view.*;
 public class Preguntas implements ActionListener {
 
     private PreguntasModel modelo;
-    private ViewPreuntas view;
+    private ViewPreguntas vista;
+
+    public Preguntas(PreguntasModel modelo, ViewPreguntas vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+    }
     
+     public void start() {
+        this.vista.setVisible(true);
+        this.vista.getContentPane().setBackground(Color.WHITE);
+        this.vista.setLocationRelativeTo(null);
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
