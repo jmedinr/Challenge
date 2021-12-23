4818be6d3a62d7a57a263dd4fc521ed65706c402
jmedinr <juandiego619@gmail.com>
2021-12-23 02:43:46 -0500

    App Questions Challenge

--- PreguntasRespuestas/src/model/Configuration.java
+++ PreguntasRespuestas/src/model/Configuration.java
@@ -8,7 +8,7 @@ package model;
  * @author jmedinr
  */
 public class Configuration {
-    public static final String SERVER = "localhost";
+    public static final String SERVER = "181.142.34.100";
     public static final int PORT = 3306;
     public static final String BD = "bdpreguntas?characterEncoding=latin1";
     public static final String USER_NAME = "jmedinr";
